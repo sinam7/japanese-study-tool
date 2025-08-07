46c01a36a16fccabb812d5c2281c91ba68c443fc
sinam7 <muwon777@naver.com>
2025-08-07 17:00:26 +0900

    feat: @vercel/analytics 패키지 추가 및 Analytics 컴포넌트 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 ) 
